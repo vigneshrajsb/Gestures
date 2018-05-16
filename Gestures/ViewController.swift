43baadbb1de23b225c2c24b5cc9d53cb5c400150
Vigneshraj Sekar Babu <vigneshrajsekarbabu@vigneshrajs-macbook-pro.local>
2018-05-16 01:01:28 -0500

    tried pan, pinch, rotate and tap gestures. tried gestures with IDE and code.  can add more practice code trying new things

--- Gestures/ViewController.swift
+++ Gestures/ViewController.swift
@@ -7,19 +7,86 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
+    
 
+ 
+    @IBOutlet weak var bananaView: UIImageView!
+    @IBOutlet weak var monkeyView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        loadGestures(view: monkeyView)
+        loadGestures(view: bananaView)
+ 
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func loadGestures(view : UIView) {
+        let tapGesture =  UITapGestureRecognizer(target: self,
+                                                 action:#selector(tapGestureHandler))
+        
+      view.addGestureRecognizer(tapGesture)
+        
     }
+    
+    
+    
+    @IBAction func panGestureHandler(_ sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: self.view)
+        if let view = sender.view {
+            view.center = CGPoint(x: view.center.x + translation.x,
+                                  y: view.center.y + translation.y)
+        }
+        sender.setTranslation(CGPoint.zero, in: self.view)
+        
+        if sender.state == .ended {
+            if view.frame.intersects(monkeyView.frame) {
+                UIView.animate(withDuration: 0.3) {
+                    self.bananaView.alpha = 0.0
+                }
+            }
+        }
+        
+    }
+    
+    @IBAction func pinchGestureHandler(_ sender: UIPinchGestureRecognizer) {
+        if let view = sender.view {
+            view.transform = view.transform.scaledBy(x: sender.scale, y: sender.scale)
+            
+        }
+        sender.scale = 1
+        
+    }
+    
+    @IBAction func rotateGestureHandler(_ sender: UIRotationGestureRecognizer) {
+        if let view = sender.view {
+            view.transform = view.transform.rotated(by: sender.rotation)
+        }
+        sender.rotation = 0
+    }
+    
+    @IBAction func tapGestureHandler(_ sender: UITapGestureRecognizer) {
+    
+        print("tap method")
+ 
+    }
+    
+    // UI Gesture delegate method to use simultaneous gestures
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        if (gestureRecognizer is UIPanGestureRecognizer || gestureRecognizer is UIRotationGestureRecognizer) {
+           // print("multiple")
 
-
+        return true
+            
+        } else {
+            
+            return false
+         
+        }
+    }
+    
 }
+
 
